37d520874476a5375ee1a34731654822550aea0e
Santosh Bhor <125242679+sbhor007@users.noreply.github.com>
2024-10-09 23:08:29 +0530

    Update Driver.java
    update menu driven

--- Class Work/Day 27 practice JDBC SQL/AccountData/Driver.java
+++ Class Work/Day 27 practice JDBC SQL/AccountData/Driver.java
@@ -24,23 +24,20 @@ public class Driver {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		TransactionType tType[] = TransactionType.values();
+		Months months[] = Months.values();
 		char goMenu;
 		do {
-			
-			System.out.print("1. Insert Data"
-					+ "\n2. Get Transaction Type Information"
-					+ "\n3. Get Monthly Trasaction Details"
-					+ "\n4. Get all transaction by specific account Number"
-					+ "\n5. Get Transaction BelowAmount"
-					+ "\n6. Get NON pune mumbai Account Holders"
-					+ "\n7. Get Min Max Transaction Amount in a month"
-					+ "\nEnter Your choise : ");
+
+			System.out.print("\n1. Insert Data" + "\n2. Get Transaction Type Information"
+					+ "\n3. Get Monthly Trasaction Details" + "\n4. Get all transaction by specific account Number"
+					+ "\n5. Get Transaction BelowAmount" + "\n6. Get NON pune mumbai Account Holders"
+					+ "\n7. Get Min Max Transaction Amount in a month" + "\nEnter Your choise : ");
 			int choice = scan.nextInt();
-			
-			switch(choice) {
+
+			switch (choice) {
 			case 1:
 				char ch = 'N';
-				
+
 				do {
 					try {
 						System.out.println("Enter Account Information");
@@ -52,33 +49,80 @@ public class Driver {
 						int amount = scan.nextInt();
 						System.out.println("Select a Transaction Type");
 						int i = 1;
-						for(TransactionType t : tType) {
+						for (TransactionType t : tType) {
 							System.out.println(i++ + ". " + t);
 						}
 						int optionNo = scan.nextInt();
-						String TransactionType = tType[optionNo-1].toString();
-						
+						String TransactionType = tType[optionNo - 1].toString();
+
 						System.out.println("Enter Date (YYYY-MM-DD)");
 						String date = scan.next();
 						// TODO : get System Date
-						
+
 						System.out.println("Address");
 						String address = scan.next();
-						
-						Account.insertRecord(accountNo, accountHolderName, amount, TransactionType, Date.valueOf(date), address);	
-						
-					}catch (InputMismatchException e) {
+
+						Account.insertRecord(accountNo, accountHolderName, amount, TransactionType, Date.valueOf(date),
+								address);
+
+					} catch (InputMismatchException e) {
 						System.out.println(e.getMessage());
 					}
 					System.out.println("Do You Want To Add Another record (Y/N): ");
 					ch = scan.next().toUpperCase().charAt(0);
-				}while(ch == 'Y');
+				} while (ch == 'Y');
+				break;
+			case 2:
+				System.out.println("Select a Transaction Type");
+				int i = 1;
+				for (TransactionType t : tType) {
+					System.out.println(i++ + ". " + t);
+				}
+				System.out.print("Enter Your Choice : ");
+				int optionNo = scan.nextInt();
+				String TransactionType = tType[optionNo - 1].toString();
+				Account.getTransactionTypeInformation(TransactionType);
+				break;
+			case 3:
+				i = 1;
+				for (Months month : months) {
+					System.out.println(i++ + ". " + month);
+				}
+				System.out.print("Select Month : ");
+				int monthNo = scan.nextInt();
+				String month = months[monthNo - 1].toString();
+				Account.getMonthlyTransactionDetails(month);
+				break;
+			case 4:
+				System.out.print("Enter Account Number : ");
+				int accountNo = scan.nextInt();
+				Account.getAllTrnsactionByAccountNo(accountNo);
+				break;
+			case 5:
+				System.out.print("Enter Ammount : ");
+				int amount = scan.nextInt();
+				Account.getTransactionsBelowAmount(amount);
+				break;
+			case 6:
+				Account.getNonPuneMumbaiAccountHolders();
+				break;
+			case 7:
+				i = 1;
+				for (Months m : months) {
+					System.out.println(i++ + ". " + m);
+				}
+				System.out.print("Select Month : ");
+				monthNo = scan.nextInt();
+				month = months[monthNo - 1].toString();
+				Account.getMinMaxTransactionAmountInMonth(month);
 				break;
 			default:
 				System.out.println("Invalid input");
 			}
-		
-		}while(false);		
-		
+			
+			System.out.print("Go To The Main Mainu (Y/N) : ");
+			goMenu = scan.next().toUpperCase().charAt(0);
+
+		} while (goMenu == 'Y');
 	}
 }
